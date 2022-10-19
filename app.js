ea6d707767c2dc42010e1e607cd28c79f137e40a
acandy212 <candies_amanda@columbusstate.edu>
2022-10-19 15:54:54 +0000

    updated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var mongoose = require('mongoose')
+var mongoose = require('mongoose')
 var axios = require('axios')
 var app = express();
 // const bodyParser = require('body-parser');
@@ -10,11 +10,11 @@ var app = express();
 app.use('/static', express.static("public"));
 app.set("view engine", "ejs");
 const Todo = require('./models/todo.model');
-// const mongoDB = 'mongodb+srv://candies_amanda:Ndx2dZV7I5rUTaJv@cluster0.godqah8.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect(mongoDB);
-// mongoose.Promise = global.Promise;
-// let db = mongoose.connection;
-// db.on('error', console.error.bind(console,"MongoDb connection error: "))
+const mongoDB = 'mongodb+srv://candies_amanda:Ndx2dZV7I5rUTaJv@cluster0.godqah8.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console,"MongoDb connection error: "))
 
 
 //Use this site to figure out how to pull data using axios
@@ -25,15 +25,21 @@ const Todo = require('./models/todo.model');
 app.get('/', function(req, res){
     let comicData = {}
     axios.get('https://xkcd.com/614/info.0.json').then(function(response){
-    Todo.find(function(err, todo){
-      console.log(todo)
-      if(err){
-          res.json({"Error: ": err})
-      } else {
-          res.render('todo.ejs', {todoList: todo, comicData: response.data});
+      
+      function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
       }
-  })
-  function getRandomInt(max) {
+      
+      console.log(getRandomInt(2682));
+      // expected output: Random number between 1 and 2682
+     
+    }).catch(function(error){
+        res.json({"Error: ": error})
+      })
+
+    // Todo.find(function(err, todo){
+    //   console.log(todo)
+    //   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
   
@@ -43,8 +49,19 @@ app.get('/', function(req, res){
 }).catch(function(error){
     res.json({"Error: ": error})
   })
+  })
+//   function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+  
+//   console.log(getRandomInt(2682));
+//   // expected output: Random number between 1 and 2682
+ 
+// }).catch(function(error){
+//     res.json({"Error: ": error})
+//   })
     
-})
+// })
 
 
 app.listen(3000, function(){
