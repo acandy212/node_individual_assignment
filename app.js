3e2624fd77887277adfa4c16cf5559e7428e5965
acandy212 <candies_amanda@columbusstate.edu>
2022-10-20 17:57:41 +0000

    updated random comic

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ var express = require('express');
 var mongoose = require('mongoose')
 var axios = require('axios')
 var app = express();
-// const bodyParser = require('body-parser');
-
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(bodyParser.json());
 
 app.use('/static', express.static("public"));
 app.set("view engine", "ejs");
-const Todo = require('./models/todo.model');
+ const Todo = require('./models/todo.model');
 const mongoDB = 'mongodb+srv://candies_amanda:Ndx2dZV7I5rUTaJv@cluster0.godqah8.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -24,34 +20,24 @@ db.on('error', console.error.bind(console,"MongoDb connection error: "))
 
 app.get('/', function(req, res){
   let comicData = {}
-  axios.get('https://xkcd.com/614/info.0.json ').then(function(response){
+  randComic = Math.floor(Math.random()*2682)+1
+  String(randComic)
+  console.log(randComic)
+  axios.get('https://xkcd.com/'+ randComic +'/info.0.json ').then(function(response){
     Todo.find(function(err, todo){
         console.log(todo)
           if(err){
           res.json({"Error: ": err})
           } else {
-          res.render('todo.ejs', {todoList: todo, comicData: response.data});
+          res.render('todo.ejs', {comicData: response.data});
           }
       })
     }).catch(function(error){
          res.json({"Error: ": error})
           })
-          
       
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-  console.log(getRandomInt(2682));
-  //expected output: Random number between 1 and 2682
 })
  
-// }).catch(function(error){
-//     res.json({"Error: ": error})
-//   })
-    
-//  })
-
 
 app.listen(3000, function(){
     console.log('App listen on port 3000')
--- models/todo.model.js
+++ models/todo.model.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let TodoSchema = new Schema({
-    todo:{type: String, required: true, max: 100},
-    done:{type: Boolean, required: true}
+    todo:{type: String, required: true,max:100},
+    done:{type:Boolean, required:true}
+
+
 })
 
 module.exports = mongoose.model('Todo', TodoSchema)
